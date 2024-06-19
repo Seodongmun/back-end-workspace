fcbb1811166ca456bd910de15a92cd376581371d
SEO <sdm0875@naver.com>
2024-06-19 21:25:04 +0900

    24/06/19

--- 1_MySQL/실습문제1_SELECT.sql
+++ 1_MySQL/실습문제1_SELECT.sql
@@ -8,10 +8,10 @@ WHERE first_name LIKE 'A%';
 -- rating이 PG면서 영화 제목(title)에 GO가 포함되는 영화 제목 조회
 SELECT title
 FROM film_list
-WHERE category in ('Sci-Fi') or ('Family') 
-		and rating in ('PG')
-        and title in('GO')
-ORDER BY 1;
+WHERE category in ('Sci-Fi','Family') and
+	  rating in ('PG') and
+	  title like '%GO%';
+
 -- 3. film_list 테이블에서 fid가 7 이하면서 4, 6은 아닌 fid, title 조회
 SELECT fid,title
 FROM film_list 
@@ -21,26 +21,28 @@ ORDER BY 1;
 -- 배우들(actors) 중 BOB가 포함되어 있는 영화 제목(title)만 조회 
 SELECT title
 FROM film_list
-WHERE 2 <= price and price >= 4 and category = 'Documentary' or 'Animation'
-		and actors = 'BOB'
+WHERE 2 <= price and price <= 4 and category in ('Documentary','Animation')
+		and actors like '%BOB%'
 ORDER BY 1;
 -- 5. address 테이블에서 district가 비어있지 않고 앞에 숫자 제외 주소만 10개 조회
--- SELECT address
--- FROM address
--- WHERE district is not null and substr(address,) 
+ SELECT address
+ FROM address
+ WHERE district is not null and trim(LEADING ' ' FROM address);
 -- 6. customer_list 테이블에서 id가 6인 사람부터 10명 조회
 SELECT id,name
 FROM customer_list
 WHERE id BETWEEN '6' and '15'
 ORDER BY 1;
 -- 7. actor 테이블에서 J로 시작하는 이름과 글자수 조회 (공백 X, 정렬은 글자수가 많은 사람 순으로)
-SELECT name,
+SELECT  case when first_name like 'j%' then concat('first_name', 'last_name')
+        end
 FROM actor
-WHERE name between 'j' and 'z';
+ORDER BY 1 desc;
+ 
 -- 8. film 테이블에서 description에서 of 이전 문장만 중복 없이 10개만 추출해서 조회
-SELECT 
-FROM film
-WHERE
+SELECT description  like '%of'
+FROM film;
+
 -- 9. film 테이블에서 replacement_cost 최소 비용과 최대 비용 조회
 SELECT min(replacement_cost) 최소비용, max(replacement_cost) 최대비용
-FROM film
+FROM film;
